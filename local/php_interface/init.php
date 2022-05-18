4f123abcbb50069fd3a6edd8bb753c95b19cf62a
Евгений Дмитриев <johndmitriev@ya.ru>
2022-05-18 22:51:20 +0700

    Тестовый коммит

---
+++ local/php_interface/init.php
@@ -0,0 +1,14 @@
+<?php
+
+function x($obj) {
+    echo '-=<xmp>';
+    print_r($obj);
+    echo '</xmp>=-';
+}
+
+function xe($obj) {
+    echo '-=<xmp>';
+    print_r($obj);
+    echo '</xmp>=-';
+    die();
+}
